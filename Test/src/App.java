d0f7fd75d6047a1e0e3430cdd91410e0991678d0
khiro <hiroki.hiroki@icloud.com>
2022-12-23 17:12:24 +0900

    オブジェクトの例

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+class App {
+    public static void main(String[] args) {
+        Pref aichiPref = new Pref("愛知県");
+        aichiPref.Hello();
+
+        Pref gihuPref = new Pref("岐阜県");
+        gihuPref.Hello();
+
+        Pref miePref = new Pref("三重県");
+        miePref.Hello();
     }
 }
---
+++ Test/src/Pref.java
@@ -0,0 +1,11 @@
+class Pref {
+    public String name;
+
+    Pref(String name) {
+        this.name = name;
+    }
+
+    public void Hello() {
+        System.out.println(this.name + "へようこそ");
+    }
+}
