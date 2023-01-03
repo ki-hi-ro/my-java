ab3b1aa6b4ed84b1ddd7dd1d697c98f42716e170
khiro <hiroki.hiroki@icloud.com>
2023-01-03 14:27:51 +0900

    追加する男性の人数を入力できるようにした

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        new City("名古屋");
+        City city = new City("名古屋", 2296000, 1071228);
+        System.out.print("男性の人口はどれだけ増えましたか？:");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int i = scanner.nextInt();
+            city.add_men(i);
+        }
     }
 }
--- Test/src/City.java
+++ Test/src/City.java
@@ -1,8 +1,17 @@
 public class City {
-    private String name;
+    private int men = 1071228;
 
-    public City(String name) {
+    public City(String name, int population, int men) {
         System.out.println("【都市の情報】");
         System.out.println("名前:" + name);
+        System.out.println("人口:" + String.format("%,d", population) + "人");
+        System.out.println("男性の人口:" + String.format("%,d", men) + "人");
+    }
+
+    public void add_men(int add_men) {
+        System.out.println("-------------------------");
+        System.out.println("男性の人口が" + String.format("%,d", add_men) + "人増えました");
+        men += add_men;
+        System.out.println("男性の人口:" + String.format("%,d", men) + "人");
     }
 }
