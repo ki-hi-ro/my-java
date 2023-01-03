defe7a0d9025b346d6e3d9a1800607fdb753fece
khiro <hiroki.hiroki@icloud.com>
2023-01-03 09:48:45 +0900

    インスタンスを生成した

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,12 +1,5 @@
 class App {
     public static void main(String[] args) {
-        Pref aichiPref = new Pref("愛知県");
-        aichiPref.Hello();
-
-        Pref gihuPref = new Pref("岐阜県");
-        gihuPref.Hello();
-
-        Pref miePref = new Pref("三重県");
-        miePref.Hello();
+        new City("名古屋");
     }
 }
---
+++ Test/src/City.java
@@ -0,0 +1,8 @@
+public class City {
+    private String name;
+
+    public City(String name) {
+        System.out.println("【都市の情報】");
+        System.out.println("名前:" + name);
+    }
+}
